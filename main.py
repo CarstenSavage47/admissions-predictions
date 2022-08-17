a1489b716c7d1adeaa6e94a12574955d62d9e7dc
CarstenSavage47 <rampagev2@gmail.com>
2022-08-16 21:46:20 -0700

    Seventh Commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # The purpose of this neural network is to predict whether a college is selective or not based on attributes.
 # The current model is using 75th percentile ACT scores, admittance rate percentages,
 # ... total enrollment, total out-of-state price, percent of enrollment that is non-white,
+# ... historically black university status (dummy variable),
 # ... and percent of total enrollment that is women as predictors for whether a college is selective.
 # I defined a 'selective' university as one that has an acceptance rate of less than 65%.
 
